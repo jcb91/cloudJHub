7545ccbdfabce21b42a298a7d7d0574e81aadc2c
jcb91 <joshuacookebarnes@gmail.com>
2018-04-27 15:50:46 +0100

    whitespace & autopep8

--- launch_cluster/launch.py
+++ launch_cluster/launch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-This sets up a JypyterHub cluster.  This script requires Python 2.7 due to Fabric
-not being Python 3 compatible
+Sets up a JypyterHub cluster.
+This script requires Python 2.7 due to Fabric not being Python-3 compatible
 
   Author: Zagaran, Inc. <info@zagaran.com>
     --
@@ -9,18 +9,17 @@ not being Python 3 compatible
       <eli@zagaran.com>
       <zags@zagaran.com>
 """
-import json
 import argparse
-import boto3
 import json
 import logging
 import os
 import sys
 from time import sleep
-from botocore.exceptions import ClientError, WaiterError
-from fabric.api import env, run, put, sudo
-from fabric.exceptions import NetworkError
 
+import boto3
+from botocore.exceptions import ClientError, WaiterError
+from fabric.api import env, put, run, sudo
+from fabric.exceptions import NetworkError
 from secure import (AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, KEY_NAME, KEY_PATH,
                     MANAGER_IAM_ROLE, VPC_ID)
 
@@ -34,38 +33,51 @@ logging.getLogger('botocore').setLevel(logging.WARNING)
 with open("launch_cluster/instance_config.json", "r") as f:
     CONFIG_DEFAULTS = json.load(f)
 
-class RemoteCmdExecutionError(Exception): pass
 
-#global fabric config
+class RemoteCmdExecutionError(Exception):
+    pass
+
+
+# global fabric config
 env.abort_exception = RemoteCmdExecutionError
 env.abort_on_prompts = True
 
 
 def launch_manager(config):
-    """ Creates security groups, Jupyterhub manager, and worker AMI. Refer to README.md for details on what the
-        launch script does. """
-    
+    """
+    Creates security groups, Jupyterhub manager, and worker AMI.
+
+    Refer to README.md for details on what the launch script does.
+    """
+
     logger.info("collecting AWS resources")
     public_subnet = subnet_connection(config.region, config.public_subnet_id)
     ec2 = ec2_connection(config.region)
-    worker_security_group, manager_security_group, manager_security_group2 = create_server_security_groups()
+    (worker_security_group,
+     manager_security_group,
+     manager_security_group2) = create_server_security_groups()
 
     # Create worker AMI
     if not config.custom_worker_ami:
         logger.info("Creating worker AMI")
         # Run worker setup
-        worker_ami_id = make_worker_ami(config, ec2, [manager_security_group.id, manager_security_group2.id])
+        worker_ami_id = make_worker_ami(
+            config, ec2,
+            [manager_security_group.id, manager_security_group2.id])
     else:
-        logger.info("Custom worker AMI id '%s' specified. Using custom AMI to launch worker user servers." % config.custom_worker_ami)
+        logger.info("Custom worker AMI id '%s' specified. Using custom AMI to launch worker user servers." %
+                    config.custom_worker_ami)
         worker_ami_id = config.custom_worker_ami
 
     # Launch the manager
     logger.info("Launching manager instance")
-    instance = launch_server(config, ec2, [manager_security_group.id, manager_security_group2.id])
+    instance = launch_server(
+        config, ec2, [manager_security_group.id, manager_security_group2.id])
 
     # Add server tags
     availability_zone = public_subnet.availability_zone
-    instance_name = "JUPYTER_HUB_%s_%s_MANAGER" % (availability_zone.split("-")[-1], config.cluster_name)
+    instance_name = "JUPYTER_HUB_%s_%s_MANAGER" % (
+        availability_zone.split("-")[-1], config.cluster_name)
     tags = [
         {"Key": "Name", "Value": instance_name},
         {"Key": "Owner", "Value": config.server_owner},
@@ -75,7 +87,7 @@ def launch_manager(config):
     instance.wait_until_exists()
     instance.wait_until_running()
     instance.create_tags(Tags=tags)
-    
+
     # Configure fabric
     env.host_string = instance.public_ip_address
     env.key_filename = KEY_PATH
@@ -86,7 +98,8 @@ def launch_manager(config):
     retry(run, "# waiting for ssh to be connectable...", max_retries=100)
 
     # These parameters will be used by the manager to launch a worker
-    worker_server_name = "JUPYTER_HUB_%s_%s_WORKER" % (availability_zone.split("-")[-1], config.cluster_name)
+    worker_server_name = "JUPYTER_HUB_%s_%s_WORKER" % (
+        availability_zone.split("-")[-1], config.cluster_name)
 
     server_params = {
         "REGION": config.region,
@@ -119,21 +132,23 @@ def launch_manager(config):
     print("Launch script done.")
 
 
-def setup_manager(server_params,config, manager_ip_address):
+def setup_manager(server_params, config, manager_ip_address):
     """ Sets up the files that are common to both workers and the manager,
         runs before worke and jupyterhub setup. """
     put("common_files", remote_path="/var/tmp/")
     # upload key to manager for usage of SSHing into worker servers
-    put(KEY_PATH, remote_path="/home/%s/.ssh/%s" % (server_params["SERVER_USERNAME"], KEY_NAME))
-    sudo("chmod 600 /home/%s/.ssh/%s" % (server_params["SERVER_USERNAME"], KEY_NAME))
-    # bash environment configuration files (for devs and admins)worker_security_group
+    put(KEY_PATH, remote_path="/home/%s/.ssh/%s" %
+        (server_params["SERVER_USERNAME"], KEY_NAME))
+    sudo("chmod 600 /home/%s/.ssh/%s" %
+         (server_params["SERVER_USERNAME"], KEY_NAME))
+    # bash environment configuration files (for devs and admins) worker_security_group
     run("cp /var/tmp/common_files/.inputrc ~/")
     run("cp /var/tmp/common_files/.bash_profile ~/")
     # Common installs: python 3
     sudo("apt-get -qq -y update")
     #sudo("apt-get -qq -y install -q python3.4 python3-pip sqlite sysv-rc-conf", quiet=True)
     sudo("apt-get -qq -y install -q python3-pip sqlite sysv-rc-conf", quiet=True)
-    sudo ("/usr/bin/pip3 install --force-reinstall --upgrade pip")
+    sudo("/usr/bin/pip3 install --force-reinstall --upgrade pip")
     #sudo("easy_install3 pip", quiet=True)
     sudo("pip3 --quiet install ipython nbgrader", quiet=True)
     # Sets up jupyterhub components
@@ -151,7 +166,8 @@ def setup_manager(server_params,config, manager_ip_address):
     sudo("systemctl daemon-reload")
     sudo("sysv-rc-conf --level 5 jupyterhub on")
     # Put the server_params dict into the environment
-    sudo("echo '%s' > /etc/jupyterhub/server_config.json" % json.dumps(server_params))
+    sudo("echo '%s' > /etc/jupyterhub/server_config.json" %
+         json.dumps(server_params))
     # Generate a token value for use in making authenticated calls to the jupyterhub api
     # Note: this value cannot be put into the server_params because the file is imported in our spawner
     sudo("/usr/local/bin/jupyterhub token -f /etc/jupyterhub/jupyterhub_config.py __tokengeneratoradmin > /etc/jupyterhub/api_token.txt")
@@ -160,13 +176,19 @@ def setup_manager(server_params,config, manager_ip_address):
     # move our cron script into place
     sudo("cp /etc/jupyterhub/jupyterhub_cron.txt /etc/cron.d/jupyterhub_cron")
     if not config.custom_worker_ami:
-        logger.info("Manager server successfully launched. Please wait 15 minutes for the worker server AMI image to become available. No worker servers (and thus, no user sessions) can be launched until the AMI is available.")
+        logger.info(
+            "Manager server successfully launched. "
+            "Please wait 15 minutes for the worker server AMI image "
+            "to become available. "
+            "No worker servers (and thus, no user sessions) "
+            "can be launched until the AMI is available.")
     # TODO: generate ssl files and enable jupyterhub ssl
 
 
 def make_worker_ami(config, ec2, security_group_list):
     """ Sets up worker components, runs before jupyterhub setup, after common setup. """
-    instance = launch_server(config, ec2, security_group_list, size=int(config.worker_ebs_size))
+    instance = launch_server(
+        config, ec2, security_group_list, size=int(config.worker_ebs_size))
     instance.wait_until_exists()
     instance.wait_until_running()
 
@@ -181,16 +203,13 @@ def make_worker_ami(config, ec2, security_group_list):
     sudo("apt-get -qq -y update")
     sudo("apt-get -qq -y install -q python python-setuptools python-dev")
     sudo("easy_install pip")
-    sudo ("apt-get -qq -y install -q python3-pip sqlite sysv-rc-conf")
-    sudo ("pip3 install --force-reinstall --upgrade pip")
-
-
+    sudo("apt-get -qq -y install -q python3-pip sqlite sysv-rc-conf")
+    sudo("pip3 install --force-reinstall --upgrade pip")
 
     put("jupyterhub_files/requirements_jupyterhub.txt", remote_path="/var/tmp/")
     # pip installs
     sudo("pip3 install --quiet -r /var/tmp/requirements_jupyterhub.txt")
     # apt-get installs for jupyterhub
-
 
     sudo("pip3 --quiet install ipython jupyter ipykernel nbgrader")
     sudo("pip2 install ipykernel --upgrade")
@@ -217,9 +236,10 @@ def make_worker_ami(config, ec2, security_group_list):
     instance.terminate()
     return worker_ami.id
 
-######################################################################################################################
-################################################## AWS HELPERS #######################################################
-######################################################################################################################
+##############################################################################
+#                                  AWS HELPERS                               #
+##############################################################################
+
 
 def create_security_group(name):
     security_group = ec2_connection(config.region).create_security_group(
@@ -259,16 +279,18 @@ def subnet_connection(region, subnet_id):
 def create_server_security_groups():
     # Create a security group for the manager with ports 22, 80, and 443 open to the public
     logger.info("Creating security groups")
-    manager_security_group_name = "jupyter-hub-%s-manager" % config.cluster_name
+    manager_security_group_name = (
+        "jupyter-hub-%s-manager" % config.cluster_name)
+
     manager_security_group = create_security_group(manager_security_group_name)
     manager_security_group.authorize_ingress(
-            FromPort=22, ToPort=22, IpProtocol="TCP", CidrIp="0.0.0.0/0"
+        FromPort=22, ToPort=22, IpProtocol="TCP", CidrIp="0.0.0.0/0"
     )
     manager_security_group.authorize_ingress(
-            FromPort=80, ToPort=80, IpProtocol="TCP", CidrIp="0.0.0.0/0"
+        FromPort=80, ToPort=80, IpProtocol="TCP", CidrIp="0.0.0.0/0"
     )
     manager_security_group.authorize_ingress(
-            FromPort=443, ToPort=443, IpProtocol="TCP", CidrIp="0.0.0.0/0"
+        FromPort=443, ToPort=443, IpProtocol="TCP", CidrIp="0.0.0.0/0"
     )
 
     worker_security_group_name = "jupyter-hub-%s-worker" % config.cluster_name
@@ -281,13 +303,14 @@ def create_server_security_groups():
     # Create a separate manager security group so that groups do not have cyclic
     # reference, in order to make deleting easier
     manager_security_group_name = "jupyter-hub-%s-manager2" % config.cluster_name
-    manager_security_group2 = create_security_group(manager_security_group_name)
+    manager_security_group2 = create_security_group(
+        manager_security_group_name)
     manager_security_group2.authorize_ingress(IpPermissions=[
-        {#Jupyterhub proxy
+        {  # Jupyterhub proxy
             "IpProtocol": "TCP", "ToPort": 8888, "FromPort": 8888,
             "UserIdGroupPairs": [{"GroupId": worker_security_group.id}]
         },
-        {#jupyterhub API
+        {  # jupyterhub API
             "IpProtocol": "TCP", "ToPort": 8081, "FromPort": 8081,
             "UserIdGroupPairs": [{"GroupId": worker_security_group.id}]
         },
@@ -296,15 +319,19 @@ def create_server_security_groups():
 
 
 def launch_server(config, ec2, security_groups_list, size=8):
-    # if we need more storage, these are parameters for BlockDeviceMappings. AWS default for EBS-backed instances is 8GB.
-    # Specifying a smaller volume size requires a custom AMI of that particular size, or AWS will throw an error.
-    boot_drive = {'DeviceName': '/dev/sda1',  # this is to be the boot drive
-                  'Ebs': {'VolumeSize': size,  # size in gigabytes
-                          'DeleteOnTermination': True,
-                          'VolumeType': 'gp2',  # This means General Purpose SSD
-                          # 'Iops': 1000 }  # i/o speed for storage, default is 100, more is faster
-                          }
-                  }
+    # if we need more storage, these are parameters for BlockDeviceMappings.
+    # AWS default for EBS-backed instances is 8GB.
+    # Specifying a smaller volume size requires a custom AMI of that
+    # particular size, or AWS will throw an error.
+    boot_drive = {
+        'DeviceName': '/dev/sda1',  # this is to be the boot drive
+        'Ebs': {
+            'VolumeSize': size,  # size in gigabytes
+            'DeleteOnTermination': True,
+            'VolumeType': 'gp2',  # This means General Purpose SSD
+          # 'Iops': 1000  # i/o speed for storage, default is 100, more is faster
+        }
+    }
     reservation = retry(
         ec2.run_instances,
         ImageId=config.base_ami,
@@ -343,33 +370,36 @@ def validate_config():
             exit()
     else:
         print("Ignoring ssh key permissions")
-    
-    if not isinstance( config.worker_ebs_size, int ):
+
+    if not isinstance(config.worker_ebs_size, int):
         if not config.worker_ebs_size.isdigit():
             print ("EBS Volume size should be a positive integer number")
             exit()
         else:
             config.worker_ebs_size = int(config.worker_ebs_size)
-            if config.worker_ebs_size <= 0 : 
+            if config.worker_ebs_size <= 0:
                 print ("EBS Volume size should be a positive integer number")
                 exit()
-    
-    if not isinstance( config.user_home_ebs_size, int ):
+
+    if not isinstance(config.user_home_ebs_size, int):
         if not config.user_home_ebs_size.isdigit():
             print ("EBS Volume size should be a positive integer number")
             exit()
         else:
             config.user_home_ebs_size = int(config.user_home_ebs_size)
-            if config.user_home_ebs_size <= 0 : 
+            if config.user_home_ebs_size <= 0:
                 print ("EBS Volume size should be a positive integer number")
                 exit()
 
 
 def retry(function, *args, **kwargs):
-    """ Retries a function up to max_retries, waiting `timeout` seconds between tries.
-        This function is designed to retry both boto3 and fabric calls.  In the
-        case of boto3, it is necessary because sometimes aws calls return too
-        early and a resource needed by the next call is not yet available. """
+    """
+    Retry a function up to max_retries, waiting `timeout` s between tries.
+
+    This function is designed to retry both boto3 and fabric calls.  In the
+    case of boto3, it is necessary because sometimes aws calls return too
+    early and a resource needed by the next call is not yet available.
+    """
     max_retries = kwargs.pop("max_retries", 10)
     timeout = kwargs.pop("timeout", 3)
     for i in range(max_retries):
@@ -388,15 +418,22 @@ def retry(function, *args, **kwargs):
 #################################################### MAIN ###########################################################
 #####################################################################################################################
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Launches a JupyterHub cluster")
-    parser.add_argument("cluster_name", help="the name of the cluster, used for tagging aws resources")
-    parser.add_argument("base_ami", help="the AWS base AMI id used for user servers")
-    parser.add_argument("private_subnet_id", help="the AWS id of the private subnet for user servers")
-    parser.add_argument("public_subnet_id", help="the AWS id of the public subnet for manager server(s)")
+    parser = argparse.ArgumentParser(
+        description="Launches a JupyterHub cluster")
+    parser.add_argument(
+        "cluster_name", help="the name of the cluster, used for tagging aws resources")
+    parser.add_argument(
+        "base_ami", help="the AWS base AMI id used for user servers")
+    parser.add_argument(
+        "private_subnet_id", help="the AWS id of the private subnet for user servers")
+    parser.add_argument(
+        "public_subnet_id", help="the AWS id of the public subnet for manager server(s)")
     for item, default in CONFIG_DEFAULTS.items():
         flag = "--%s" % item.lower()
-        parser.add_argument(flag, help="defaults to %s" % default, default=default)
+        parser.add_argument(flag, help="defaults to %s" %
+                            default, default=default)
     config = parser.parse_args()
     validate_config()
     launch_manager(config)
